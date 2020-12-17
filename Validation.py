eedbb2f9d286cc64d1c9985bd9585f4ffd943f8d
slime0519 <slime0519@gist.ac.kr>
2020-12-17 12:56:56 +0000

    edit Validation routine

--- Validation.py
+++ Validation.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     valid_dataset = Dataset_gen.Dataset_Validation(dirpath=datasetpath, crop_size=CROP_SIZE,
                                                         upscale_factor=UPSCALE_FACTOR)
-    valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=4, shuffle=False, num_workers=2,
+    valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=1, shuffle=False, num_workers=2,
                                        pin_memory=True)
 
 
@@ -60,7 +60,8 @@ if __name__ == "__main__":
     generator.eval()
     print("validate about dataset {}".format(dataset_name))
 
-    for i,(lr_image, hr_image) in enumerate(tqdm.tqdm(valid_dataloader, bar_format="{l_bar}{bar:40}{r_bar}")):
+    num_image = 0
+    for lr_image, hr_image in tqdm.tqdm(valid_dataloader, bar_format="{l_bar}{bar:40}{r_bar}"):
 
         lr_image, hr_image = lr_image.to(device), hr_image.to(device)
 
@@ -69,11 +70,12 @@ if __name__ == "__main__":
 
         temp_fake = torch.clamp(fake_hr, min=0, max=1)
         accum_psnr += psnr(temp_fake, hr_image)
-        temp_mse = None
-        temp_fake = None
-        fake_hr = None
+        #temp_mse = None
+        #temp_fake = None
+        #fake_hr = None
 
-        save_image(temp_fake,  os.path.join(savefolderpath, "image{}.png".format(i)))
+        save_image(temp_fake[0],  os.path.join(savefolderpath, "image{}.png".format(num_image)))
+        num_image +=1
 
     validation_PSNR = accum_psnr / len(valid_dataloader)
     print("average PSNR about dataset {}: {}".format(dataset_name, validation_PSNR))
